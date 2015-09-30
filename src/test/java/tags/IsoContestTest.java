77d8b34fabe68fc0c998703d0ad566be19d70601
julien <girardot.jul@gmail.com>
2015-09-30 22:01:07 +0200

    tags test + impl

--- src/test/java/tags/IsoContestTest.java
+++ src/test/java/tags/IsoContestTest.java
@@ -13,15 +13,21 @@ public class IsoContestTest extends AbstractTest {
     @Test
     public void should_return_money_at_the_end_of_the_game() throws Exception {
         // Given
-        String data = "15\n" +
+        String data = "21\n" +
                 "tag1\n" +
                 "tag4\n" +
+                "tag1\n" +
                 "tag3\n" +
                 "tag5\n" +
+                "tag4\n" +
+                "tag5\n" +
                 "tag1\n" +
+                "tag3\n" +
                 "tag2\n" +
                 "tag4\n" +
+                "tag42\n" +
                 "tag1\n" +
+                "tag2\n" +
                 "tag2\n" +
                 "tag1\n" +
                 "tag3\n" +
@@ -36,7 +42,13 @@ public class IsoContestTest extends AbstractTest {
         tags.IsoContest.main(null);
 
         //Then
-        assertThat(outContent.toString().trim()).isEqualTo("tag1 5\n tag2 4\ntag3 3 \n tag 4 2\n tag 5 1\n");
+        assertThat(outContent.toString()).isEqualTo(
+                "tag1 6\r\n" +
+                "tag2 5\r\n" +
+                "tag3 4\r\n" +
+                "tag4 3\r\n" +
+                "tag5 2\r\n"
+        );
     }
 
 }
