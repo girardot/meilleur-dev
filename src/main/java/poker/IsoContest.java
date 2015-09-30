a8463b483a096f3aec6d932f604c4feb2c2b46b6
julien <girardot.jul@gmail.com>
2015-09-30 13:36:03 +0200

    poker implementation

--- src/main/java/poker/IsoContest.java
+++ src/main/java/poker/IsoContest.java
@@ -14,10 +14,17 @@ public class IsoContest {
     public static void main(String[] argv) throws Exception {
         String line;
         Scanner sc = new Scanner(System.in);
+        int playerMoney = Integer.parseInt(sc.nextLine());
+        int turnNumber = Integer.parseInt(sc.nextLine());
+
         while (sc.hasNextLine()) {
             line = sc.nextLine();
-            /* Lisez les données et effectuez votre traitement */
+            String[] splitLine = line.split(" ");
+            int x = Integer.parseInt(splitLine[0]);
+            int y = Integer.parseInt(splitLine[1]);
+            playerMoney += -x + y;
         }
-	/* Vous pouvez aussi effectuer votre traitement une fois que vous avez lu toutes les données.*/
+        System.out.println(playerMoney);
+
     }
 }
