7230b53adff81c2e4038c8e6e75cd374134ab75c
julien <girardot.jul@gmail.com>
2015-10-01 14:00:37 +0200

    tags test with inout output files

--- src/test/java/tags/IsoContestTest.java
+++ src/test/java/tags/IsoContestTest.java
@@ -1,54 +1,31 @@
 package tags;
 
 import common.AbstractTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class IsoContestTest extends AbstractTest {
 
-
     @Test
-    public void should_return_money_at_the_end_of_the_game() throws Exception {
+    @Parameters({
+            "src/test/resources/tags/input1.txt,src/test/resources/tags/output1.txt",
+            "src/test/resources/tags/input2.txt,src/test/resources/tags/output2.txt"})
+    public void should_return_money_at_the_end_of_the_game(String inputFileName, String outputFileName) throws Exception {
         // Given
-        String data = "21\n" +
-                "tag1\n" +
-                "tag4\n" +
-                "tag1\n" +
-                "tag3\n" +
-                "tag5\n" +
-                "tag4\n" +
-                "tag5\n" +
-                "tag1\n" +
-                "tag3\n" +
-                "tag2\n" +
-                "tag4\n" +
-                "tag42\n" +
-                "tag1\n" +
-                "tag2\n" +
-                "tag2\n" +
-                "tag1\n" +
-                "tag3\n" +
-                "tag2\n" +
-                "tag2\n" +
-                "tag3\n" +
-                "tag1\n";
-
-        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        System.setIn(new FileInputStream(inputFileName));
 
         // When
         tags.IsoContest.main(null);
 
         //Then
-        assertThat(outContent.toString()).isEqualTo(
-                "tag1 6\r\n" +
-                "tag2 5\r\n" +
-                "tag3 4\r\n" +
-                "tag4 3\r\n" +
-                "tag5 2\r\n"
-        );
+        assertThat(outContent.toString().trim()).isEqualTo(readOutputFile(outputFileName));
     }
 
 }
