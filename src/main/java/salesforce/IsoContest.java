0c59b9f2109d7d2ed799ad3f194edf0f0cf26cf8
julien <girardot.jul@gmail.com>
2015-10-01 18:10:11 +0200

    sales force impl

--- src/main/java/salesforce/IsoContest.java
+++ src/main/java/salesforce/IsoContest.java
@@ -8,16 +8,55 @@ package salesforce;
  * to display variable in a dedicated area.
  ****/
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class IsoContest {
     public static void main(String[] argv) throws Exception {
         String line;
         Scanner sc = new Scanner(System.in);
+        int numberOfLine = Integer.parseInt(sc.nextLine());
+        List<String> countries = Arrays.asList(sc.nextLine().split(";"));
+
+        int x;
+        int y = 0;
+        int z = 0;
+
+        Set<String> lines = new HashSet<String>();
         while (sc.hasNextLine()) {
             line = sc.nextLine();
-            /* Lisez les données et effectuez votre traitement */
+            lines.add(line);
         }
-    /* Vous pouvez aussi effectuer votre traitement une fois que vous avez lu toutes les données.*/
+
+        x = numberOfLine - lines.size();
+
+        for (String l : lines) {
+            String[] user = l.split(";");
+            String phone = user[3];
+            if (!validatePhoneNumber(phone)) {
+                y++;
+            }
+
+            String country = user[4];
+            if (!validateCountry(country, countries)) {
+                z++;
+            }
+        }
+
+        System.out.println(x + " " + y + " " + z);
     }
+
+    private static boolean validatePhoneNumber(String phone) {
+        final String PHONE_REGEX = "^\\+(?:[0-9]\\-?){6,14}";
+        final Pattern phonePattern = Pattern.compile(PHONE_REGEX);
+        Matcher matcher = phonePattern.matcher(phone);
+        return matcher.matches();
+    }
+
+    private static boolean validateCountry(String country, List<String> countries) {
+        return countries.contains(country);
+    }
+
+
 }
