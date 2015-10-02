cdc7fe0a8a1d59338f4d2fd56a3fee626ff01b10
julien <girardot.jul@gmail.com>
2015-10-02 12:52:29 +0200

    trending topics

--- src/main/java/trending/topics/IsoContest.java
+++ src/main/java/trending/topics/IsoContest.java
@@ -1,5 +1,8 @@
 package trending.topics;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class IsoContest {
@@ -8,12 +11,38 @@ public class IsoContest {
         Scanner sc = new Scanner(System.in);
         int hashTagNumber = Integer.parseInt(sc.nextLine());
 
+        int i = 0;
+        List<String> hashtags = new ArrayList<>(hashTagNumber);
         while (sc.hasNextLine()) {
             line = sc.nextLine();
-            String hashtag = line.split("#")[1];
-
-
+            hashtags.add(line.split("#")[1]);
         }
-        System.out.println("#" + "Pas de trending topic");
+
+        String trendingTopic = null;
+
+        for (int p = 0; p < (hashTagNumber - 1 - 60); p++) {
+            HashMap<String, Integer> countHashTags = new HashMap<>();
+            for (int j = 0; j < 60; j++) {
+                String hashtag = hashtags.get(j + p);
+
+                if (countHashTags.get(hashtag) == null) {
+                    countHashTags.put(hashtag, 1);
+                } else if (countHashTags.get(hashtag) < 39) {
+                    countHashTags.put(hashtag, countHashTags.get(hashtag) + 1);
+                } else {
+                    trendingTopic = hashtag;
+                    break;
+                }
+            }
+            if (trendingTopic != null) {
+                break;
+            }
+        }
+
+        if (trendingTopic == null) {
+            System.out.println("#" + "Pas de trending topic");
+        } else {
+            System.out.println("#" + trendingTopic);
+        }
     }
 }
