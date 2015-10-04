2fbadd1eaa9149ad8c45d04c46b8a564e26fac8b
julien <girardot.jul@gmail.com>
2015-10-04 14:45:47 +0200

    quicksand

--- src/main/java/quicksand/IsoContest.java
+++ src/main/java/quicksand/IsoContest.java
@@ -1,8 +1,15 @@
 package quicksand;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import static java.lang.String.valueOf;
+
 public class IsoContest {
+    private static final String GROUND = ".";
+    private static final String SAND = "#";
+
     public static void main(String[] argv) throws Exception {
         String line;
         Scanner sc = new Scanner(System.in);
@@ -12,32 +19,52 @@ public class IsoContest {
         int width = Integer.parseInt(firstLine[1]);
         String field[][] = loadField(sc, height, width);
 
-        int maxDeep = 0;
+        int maxDepth = 0;
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                if (field[y][x].equals("#")) {
-                    int deep = 1;
-                    maxDeep = Math.max(maxDeep, deep);
+                if (field[y][x].equals(SAND)) {
+                    int depth = computeDepth(x, y, field, height, width);
+                    maxDepth = Math.max(maxDepth, depth);
                 }
             }
         }
-        System.out.println(maxDeep);
+        System.out.println(maxDepth);
     }
 
     private static String[][] loadField(Scanner sc, int height, int width) {
-        String line;
         String[][] field = new String[height][width];
         int y = 0;
         while (sc.hasNextLine()) {
-            line = sc.nextLine();
-            char[] charLine = line.toCharArray();
-            for (int x = 0; x < charLine.length; x++) {
-                field[y][x] = String.valueOf(charLine[x]);
+            char[] line = sc.nextLine().toCharArray();
+            for (int x = 0; x < line.length; x++) {
+                field[y][x] = valueOf(line[x]);
             }
             y++;
         }
         return field;
+    }
+
+    private static int computeDepth(int x, int y, String[][] field, int height, int width) {
+        int maxDistance = Math.min(height / 2, width / 2);
+        for (int distance = 1; distance <= maxDistance; distance++) {
+            if (getAllCellsAtDistance(x, y, distance, field, height, width).contains(GROUND)) {
+                return distance;
+            }
+        }
+        return 0;
+    }
+
+    private static List<String> getAllCellsAtDistance(int x, int y, int distance, String[][] field, int height, int width) {
+        List<String> cellsAtDistance = new ArrayList<>();
+        for (int yy = 0; yy < height; yy++) {
+            for (int xx = 0; xx < width; xx++) {
+                if (distance == Math.abs(xx - x) + Math.abs(yy - y)) {
+                    cellsAtDistance.add(field[yy][xx]);
+                }
+            }
+        }
+        return cellsAtDistance;
     }
 
 }
