9868c2c228d12508be086ad981ca71eec790eeaa
julien <girardot.jul@gmail.com>
2015-10-05 22:13:18 +0200

    substring

---
+++ src/main/java/substring/IsoContest.java
@@ -0,0 +1,16 @@
+package substring;
+
+import java.util.Scanner;
+
+public class IsoContest {
+
+    public static void main(String[] argv) throws Exception {
+        String line;
+        Scanner sc = new Scanner(System.in);
+        String x = sc.nextLine();
+        String y = sc.nextLine();
+
+        System.out.println("OK");
+    }
+
+}
