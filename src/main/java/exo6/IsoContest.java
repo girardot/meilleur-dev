0c7264a5bd2ee50da70929860f4a9370627c217b
julien <girardot.jul@gmail.com>
2015-10-05 20:58:23 +0200

    blank exo 1 to 6

---
+++ src/main/java/exo6/IsoContest.java
@@ -0,0 +1,23 @@
+package exo6;
+
+/*******
+ * Read input from System.in
+ * Use System.out.println to ouput your result.
+ * Use:
+ * IsoContestBase.localEcho( variable)
+ * to display variable in a dedicated area.
+ ***/
+
+import java.util.Scanner;
+
+public class IsoContest {
+    public static void main(String[] argv) throws Exception {
+        String line;
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            line = sc.nextLine();
+            /* Lisez les données et effectuez votre traitement */
+        }
+	/* Vous pouvez aussi effectuer votre traitement une fois que vous avez lu toutes les données.*/
+    }
+}
---
+++ src/test/java/exo6/IsoContestTest.java
@@ -0,0 +1,31 @@
+package exo6;
+
+import common.AbstractTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.FileInputStream;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+@RunWith(JUnitParamsRunner.class)
+public class IsoContestTest extends AbstractTest {
+
+    @Test
+    @Parameters({
+            "src/test/resources/exo6/input1.txt,src/test/resources/exo6/output1.txt",
+            "src/test/resources/exo6/input2.txt,src/test/resources/exo6/output2.txt"})
+    public void should_test_exo(String inputFileName, String outputFileName) throws Exception {
+        // Given
+        System.setIn(new FileInputStream(inputFileName));
+
+        // When
+        exo6.IsoContest.main(null);
+
+        //Then
+        assertThat(outContent.toString().trim()).isEqualTo(readOutputFile(outputFileName));
+    }
+
+}
