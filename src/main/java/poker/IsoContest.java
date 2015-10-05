104991da507167bc5bf41c34f6b5752fad6ffb16
julien <girardot.jul@gmail.com>
2015-10-05 21:07:15 +0200

    static import

--- src/main/java/poker/IsoContest.java
+++ src/main/java/poker/IsoContest.java
@@ -10,18 +10,20 @@ package poker;
 
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class IsoContest {
     public static void main(String[] argv) throws Exception {
         String line;
         Scanner sc = new Scanner(System.in);
-        int playerMoney = Integer.parseInt(sc.nextLine());
-        int turnNumber = Integer.parseInt(sc.nextLine());
+        int playerMoney = parseInt(sc.nextLine());
+        int turnNumber = parseInt(sc.nextLine());
 
         while (sc.hasNextLine()) {
             line = sc.nextLine();
             String[] splitLine = line.split(" ");
-            int x = Integer.parseInt(splitLine[0]);
-            int y = Integer.parseInt(splitLine[1]);
+            int x = parseInt(splitLine[0]);
+            int y = parseInt(splitLine[1]);
             playerMoney += -x + y;
         }
         System.out.println(playerMoney);
