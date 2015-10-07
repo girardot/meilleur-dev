d1beff8cffad7f1a47f05f305be16f489baf2e94
julien <girardot.jul@gmail.com>
2015-10-07 19:42:29 +0200

    substring

--- src/main/java/substring/IsoContest.java
+++ src/main/java/substring/IsoContest.java
@@ -1,16 +1,32 @@
 package substring;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class IsoContest {
-
     public static void main(String[] argv) throws Exception {
-        String line;
         Scanner sc = new Scanner(System.in);
         String x = sc.nextLine();
         String y = sc.nextLine();
 
-        System.out.println("OK");
+        int l = 0;
+        LinkedList<Character> xc = new LinkedList<>();
+        for (Character c : x.toCharArray()) {
+            xc.addLast(c);
+        }
+        for (Character c : y.toCharArray()) {
+            if (xc.isEmpty()) {
+                break;
+            }
+            if (c.equals(xc.getFirst())) {
+                xc.removeFirst();
+                l++;
+            }
+        }
+        if (l == x.length()) {
+            System.out.println("OK");
+        } else {
+            System.out.println("NOK " + l);
+        }
     }
-
 }
