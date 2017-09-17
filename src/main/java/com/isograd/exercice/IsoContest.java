f896fed1c57954213b0b5b70e652b49354b33035
julien <girardot.jul@gmail.com>
2017-09-17 10:43:25 +0200

    refactor

--- src/main/java/com/isograd/exercice/IsoContest.java
+++ src/main/java/com/isograd/exercice/IsoContest.java
@@ -10,6 +10,8 @@ package com.isograd.exercice;
 
 import java.util.Scanner;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class IsoContest {
     public static void main(String[] argv) throws Exception {
@@ -21,4 +23,67 @@ public class IsoContest {
         }
 	/* Vous pouvez aussi effectuer votre traitement une fois que vous avez lu toutes les données.*/
     }
+
+    /*******************************************************************************************/
+
+    private static void sortByReverseOrder(List<Integer> list) {
+        Collections.sort(list, COMPARE_BY_REVERSE_ORDER);
+    }
+
+    private static void sortByOrder(List<Integer> list) {
+        Collections.sort(list, COMPARE_BY_ORDER);
+    }
+
+    private static final Comparator<Integer> COMPARE_BY_REVERSE_ORDER = new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2.compareTo(o1);
+        }
+    };
+
+    private static final Comparator<Integer> COMPARE_BY_ORDER = new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o1.compareTo(o2);
+        }
+    };
+
+    /*******************************************************************************************/
+
+    private static boolean validatePhoneNumber(String phone) {
+        final String PHONE_REGEX = "^\\+[0-9]{1,3}\\-[0-9]{9,11}$";
+        final Pattern phonePattern = Pattern.compile(PHONE_REGEX);
+        Matcher matcher = phonePattern.matcher(phone);
+        return matcher.matches();
+    }
+
+    static <K, V extends Comparable<? super V>>
+    SortedSet<Map.Entry<K, V>> entriesSortedByValuesDesc(Map<K, V> map) {
+        SortedSet<Map.Entry<K, V>> sortedEntries = new TreeSet<Map.Entry<K, V>>(
+                new Comparator<Map.Entry<K, V>>() {
+                    public int compare(Map.Entry<K, V> e1, Map.Entry<K, V> e2) {
+                        int res = e2.getValue().compareTo(e1.getValue());
+                        return res != 0 ? res : 1;
+                    }
+                }
+        );
+        sortedEntries.addAll(map.entrySet());
+        return sortedEntries;
+    }
+
+
+    static <K, V extends Comparable<? super V>>
+    SortedSet<Map.Entry<K, V>> entriesSortedByValuesAsc(Map<K, V> map) {
+        SortedSet<Map.Entry<K, V>> sortedEntries = new TreeSet<Map.Entry<K, V>>(
+                new Comparator<Map.Entry<K, V>>() {
+                    public int compare(Map.Entry<K, V> e1, Map.Entry<K, V> e2) {
+                        int res = e1.getValue().compareTo(e2.getValue());
+                        return res != 0 ? res : 1;
+                    }
+                }
+        );
+        sortedEntries.addAll(map.entrySet());
+        return sortedEntries;
+    }
+
 }
--- src/test/java/com/isograd/exercice/IsoContestTest.java
+++ src/test/java/com/isograd/exercice/IsoContestTest.java
@@ -15,8 +15,13 @@ public class IsoContestTest extends AbstractTest {
 
     @Test
     @Parameters({
-            "src/test/resources/com.isograd.exercice/input1.txt,src/test/resources/com.isograd.exercice/output1.txt",
-            "src/test/resources/com.isograd.exercice/input2.txt,src/test/resources/com.isograd.exercice/output2.txt"})
+            "src/test/resources/com/isograd/exercice/input1.txt,src/test/resources/com/isograd/exercice/output1.txt",
+            "src/test/resources/com/isograd/exercice/input2.txt,src/test/resources/com/isograd/exercice/output2.txt",
+            "src/test/resources/com/isograd/exercice/input3.txt,src/test/resources/com/isograd/exercice/output3.txt",
+            "src/test/resources/com/isograd/exercice/input4.txt,src/test/resources/com/isograd/exercice/output4.txt",
+            "src/test/resources/com/isograd/exercice/input5.txt,src/test/resources/com/isograd/exercice/output5.txt",
+            "src/test/resources/com/isograd/exercice/input6.txt,src/test/resources/com/isograd/exercice/output6.txt"
+    })
     public void should_test_exo(String inputFileName, String outputFileName) throws Exception {
         // Given
         System.setIn(new FileInputStream(inputFileName));
